2f362c52ff4e549629dc18938f2ca4b4f0442563
ruriarmandhani <ruriarmandhani@gmail.com>
2016-05-20 17:41:02 +0700

    155150207111147_RuriArmandhani

---
+++ MainTebakAngka.java
@@ -0,0 +1,9 @@
+package maintebakangka;
+public class MainTebakAngka {
+    public static void main(String[] args) {
+        TebakAngka ta = new TebakAngka();
+        ta.setTitle("Game Tebak Angka");
+        ta.setSize(350,200);
+        ta.show();
+    }
+}
---
+++ TebakAngka.java
@@ -0,0 +1,42 @@
+package maintebakangka;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.JOptionPane;
+public class TebakAngka extends Frame {
+    int count;
+    Label lbJudul, lbAngka;
+    TextField txtAngka;
+    Button btnTebak;
+    public TebakAngka() {
+        setLayout(null);
+        lbJudul = new Label("Tebak Angka");
+        lbJudul.setFont(new Font("Arial", 1, 14));
+        add(lbJudul);
+        lbJudul.setBounds(120, 40, 170, 20);
+        lbAngka = new Label("Masukkan Angka (1-10) : ");
+        add(lbAngka);
+        lbAngka.setBounds(70, 80, 150, 20);
+        txtAngka = new TextField("0");
+        add(txtAngka);
+        txtAngka.setBounds(220, 80, 30, 20);
+        btnTebak = new Button("Tebak");
+        add(btnTebak);
+        btnTebak.setBounds(120, 120, 100, 20);
+        btnTebak.addActionListener(new mainAction());
+    }
+    class mainAction implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            int angka = Integer.parseInt(txtAngka.getText().trim());
+            int tebak = (int) (Math.random() * 10 + 1);
+            int i = 0;
+            count++;
+            if (angka == tebak) {
+                JOptionPane.showMessageDialog(null, "Anda Menang");
+                count = 0;
+            } else if (count == 3) {
+                JOptionPane.showMessageDialog(null, "Anda Kalah");
+                count = 0;
+            }
+        }
+    }
+}
