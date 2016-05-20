d40441a8631387781b72f26969d8d336c8362396
DickySatriawan <dicky.satriawan8@gmail.com>
2016-05-20 23:54:44 +0700

    155150200111233_DickySatriawan

---
+++ Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan9;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author DS
+ */
+public class Main {   
+    public static void main(String[] args) {
+        TebakAngka window = new TebakAngka();
+        window.setTitle("PROGRAM TEBAK ANGKA");
+        window.setSize(350, 300);
+        window.setBackground(Color.orange);
+        window.show();
+    }
+}
