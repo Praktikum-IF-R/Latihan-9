e985a3bebca3f6ecfb31ab20fd8386024af76809
akira11riza <akira7riza@gmail.com>
2016-05-21 00:20:24 +0700

    155150200111235_Aris Adi Syahputra

---
+++ GameM.java
@@ -0,0 +1,9 @@
+package bab8;
+public class GameM {
+    public static void main(String[] args) {
+        Game window = new Game();
+        window.setTitle("Game tebak tebakan.awt");
+        window.setSize(700, 250);
+        window.show(); 
+    }
+}
