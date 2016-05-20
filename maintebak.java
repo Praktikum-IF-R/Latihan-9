26bf283db8d54eec833a78fe18ce57c113d526fd
muhammad enowato <muhammad.eno97@gmail.com>
2016-05-20 17:25:10 +0700

    155150200111236_MuhammadEnowato

---
+++ maintebak.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan9;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class maintebak {
+    public static void main(String[] args) {
+        tebak tes=new tebak();
+        tes.setTitle("main tebak tebakan angka 1-10 kuy");
+        tes.setSize(400,300);
+        tes.show();
+    }
+}
