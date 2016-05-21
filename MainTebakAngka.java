99c19e9e64735427dfcc259119b50165d962d0e0
zeddinarief <zeddinarief@gmail.com>
2016-05-21 07:21:03 +0700

    155150200111234_ZEDDIN ARIEF

---
+++ MainTebakAngka.java
@@ -0,0 +1,9 @@
+package GUI;
+public class MainTebakAngka {
+    public static void main(String[] args) {
+        TebakAngka wi = new TebakAngka();
+        wi.setTitle("Yuk Tebak Angka!!!");
+        wi.setSize(300, 250);
+        wi.show();
+    } 
+}   
