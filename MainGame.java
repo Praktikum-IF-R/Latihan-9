cee879e2bc88dd52ec7bc3328f18ddae9200a0f3
ardrgtpram <ardiaregitap@gmail.com>
2016-05-20 23:13:31 -0700

    155150200111238_ArdiaRegitaPramesti

---
+++ MainGame.java
@@ -0,0 +1,10 @@
+package latihan9;
+
+public class MainGame {
+    public static void main(String[] args) {
+        GameRandom game = new GameRandom();
+        game.setTitle("Ayo Main");
+        game.setSize(300, 200);
+        game.show();
+    }
+}
