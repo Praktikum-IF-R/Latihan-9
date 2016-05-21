ad1cd37ce693571e828629a778cc000bfe597c02
nugrhosam <nugrohosamiyono@gmail.com>
2016-05-21 09:24:50 +0700

    155150201111324_NUGROHO SAMIYONO

---
+++ mainClass.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BAB9.latihan;
+
+/**
+ *
+ * @author nugroho
+ */
+public class mainClass {
+
+    public static void main(String[] args) {
+
+        GUInya window = new GUInya();
+        window.setTitle("coba keberuntungan");
+        window.setSize(350, 300);
+        window.show();
+    }
+}
