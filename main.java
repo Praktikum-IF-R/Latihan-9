53671a047d4c12f5b3443d9ab6db60079e7ee83f
hanoumevaa <hanumevahayati88@gmail.com>
2016-05-21 08:06:40 +0700

    155150200111242_HANOUM EVA HAYATI

---
+++ main.java
@@ -0,0 +1,11 @@
+package latian;
+
+public class main {
+
+    public static void main(String[] args) {
+        tebakangka window = new tebakangka();
+        window.setTitle("Guess the number");
+        window.setSize(450, 300);
+        window.show();
+    }
+}
